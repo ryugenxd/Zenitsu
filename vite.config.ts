ffa63f6217278d9c98dc3421579dd7bc4f938a04
ryugenxd <anonimous7770@gmail.com>
2024-05-23 09:04:39 +0800

    chore: remove and change origin

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    origin: 'https://mastercodebrain.com/Zenitsu',
+    origin: 'https://mastercodebrain.com/Zenitsu/',
   },
   plugins: [react()],
   
